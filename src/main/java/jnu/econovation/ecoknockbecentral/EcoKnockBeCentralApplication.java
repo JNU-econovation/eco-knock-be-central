10545fdfdb0f9c36364c21859f9c98f6092e7062
rdme0 <we6610@gmail.com>
2026-03-28 01:20:44 +0900

    :sparkles: initial commit

---
+++ src/main/java/jnu/econovation/ecoknockbecentral/EcoKnockBeCentralApplication.java
@@ -0,0 +1,13 @@
+package jnu.econovation.ecoknockbecentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoKnockBeCentralApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(EcoKnockBeCentralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jnu/econovation/ecoknockbecentral/EcoKnockBeCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package jnu.econovation.ecoknockbecentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoKnockBeCentralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
