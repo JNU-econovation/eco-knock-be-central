323a42c09954b295f2f470290a2b7431006eb102
rdme0 <we6610@gmail.com>
2026-04-02 19:56:25 +0900

    :sparkles: feat: 센서 gRPC 수신과 health 체크 추가

--- src/main/kotlin/jnu/econovation/ecoknockbecentral/grpc/config/GrpcServerConfig.kt
+++ src/main/kotlin/jnu/econovation/ecoknockbecentral/grpc/config/GrpcServerConfig.kt
@@ -3,7 +3,6 @@ package jnu.econovation.ecoknockbecentral.grpc.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "grpc.server")
-data class GrpcConfig(
+data class GrpcServerConfig(
     val port: Int,
-    val testPort: Int
 )
